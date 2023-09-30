e41793caa60406dd396cde26963f4a77040975eb
Sonu Kumar <sonukumar@Sonus-MacBook-Air.local>
2023-09-30 16:01:27 +0530

    Updating the site along with nwe design

--- js/script.js
+++ js/script.js
@@ -1,73 +1,49 @@
-$(function(){
+window.addEventListener("scroll", setScrollVar);
+window.addEventListener("resize", setScrollVar);
 
-    /*quote animation*/
-    var ids = ['#first','#second','#third','#fourth'];
-    var count = 0;
+function setScrollVar() {
+  const htmlElement = document.documentElement;
+  const percentOfScreenHeightScrolled =
+    htmlElement.scrollTop / htmlElement.clientHeight;
+  htmlElement.style.setProperty(
+    "--scroll",
+    Math.min(percentOfScreenHeightScrolled * 100, 100)
+  );
+}
 
-    setInterval(function(){
-        $('#first').css('display','none');
-        $('#second').css('display','none');
-        $('#third').css('display','none');
-        $('#fourth').css('display','none');
+setScrollVar();
 
-        switch(count) {
-            case 0:
-                $('#first').css('display','block');
-                $("#first").fadeOut(0);
-                $("#first").fadeIn(4000);
-                $("#first").fadeOut(4000);
-                
-                count = count + 1;
-                break;
-            case 1:
-                $('#second').css('display','block');
-                $("#second").fadeOut(0)
-                $("#second").fadeIn(4000);
-                $("#second").fadeOut(4000)
-               
-                count = count + 1;
-                break;
+const observer = new IntersectionObserver((entries, observer) => {
+  for (let i = entries.length - 1; i >= 0; i--) {
+    const entry = entries[i];
+    if (entry.isIntersecting) {
+      // document.querySelectorAll("[data-img]").forEach((img) => {
+      //   img.classList.remove("show");
+      // });
+      console.log(entry);
+      document.querySelector("[data-text]").classList.add("animateHeader");
+      // const img = document.querySelector(entry.target.dataset["imgToShow"]);
+      // img?.classList.add("show");
+      observer.unobserve(document.querySelector("[data-header]"));
+      break;
+    }
+  }
+});
 
-            case 2:
-                $('#third').css('display','block');
-                $("#third").fadeOut(00)
-                $("#third").fadeIn(4000);
-                $("#third").fadeOut(4000)
-               
-                count = count + 1;
-                break;
-            case 3:
-                $('#fourth').css('display','block');
-                $("#fourth").fadeOut(00)
-                $("#fourth").fadeIn(4000);
-                $("#fourth").fadeOut(4000)
-                
-                count = count + 1;
-                break;
-            default:
-                $('#first').css('display','none');
-                $('#second').css('display','none');
-                $('#third').css('display','none');
-                $('#fourth').css('display','none');
-                count = 0;
+setTimeout(() => {
+  // document.querySelectorAll("[data-img-to-show]")?.forEach((section) => {
+  //   observer.observe(section);
+  // });
+  // observer.observe(document.querySelector("[data-header]"));
+  // observer.observe(document.querySelector("[data-header2]"));
+  // observer.observe(document.querySelector("[data-header3]"));
+}, 100);
 
-        }
-    },10000) 
+// $(function(){
 
+//     $('.downloadLink').click(function(e) {
+//         e.preventDefault();  //stop the browser from following
+//         window.location.href = '../Resumenew.pdf';
+//     });
 
-
-  
-
-    $('.downloadLink').click(function(e) {
-        e.preventDefault();  //stop the browser from following
-        window.location.href = '../Resumenew.pdf';
-    });
-
-    $('.top').click(function(e) {
-        e.preventDefault();  //stop the browser from following
-        window.scrollTo(0, 0);
-    });
-    
-       
-    
-})
+// })
